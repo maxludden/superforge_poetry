01f8a9a5dea4a9521de5bddedfd8e436959907cb
Max Ludden <dev@maxludden.com>
2022-09-05 07:03:43 -0400

    pushover

--- superforge/pushover.py
+++ superforge/pushover.py
@@ -2,20 +2,21 @@ import os
 from json import loads
 
 import requests
-from requests import get, post, Response
+from requests import post
 from pprint import pprint
+from dotenv import dotenv_values, load_dotenv
 
-from dotenv import dotenv_values
-
-secret_dict = dotenv_values("env/.env")
-USER_KEY = secret_dict["USER_KEY"]
-APP_TOKEN = secret_dict["API_TOKEN"]
-
+load_dotenv()
+# secret_dict = dotenv_values("env/.env")
+# USER_KEY = secret_dict["USER_KEY"]
+# APP_TOKEN = secret_dict["API_TOKEN"]
+USER_KEY = os.environ.get("USER_KEY")
+API_TOKEN = os.environ.get("API_TOKEN")
 
 def pushover(title: str, msg: str):
     url = "https://api.pushover.net/1/messages.json"
     payload = {
-        "token": APP_TOKEN,
+        "token": API_TOKEN,
         "user": USER_KEY,
         "title": title,
         "message": msg,
@@ -24,5 +25,4 @@ def pushover(title: str, msg: str):
     if response.status_code == 200:
         result = loads(response.text)
         pprint(result)
-
-
+        pushover("SuperForge", "Backup complete")
